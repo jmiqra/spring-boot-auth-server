0869576dd45009fcaa4e76c16ad82e5e0b65e681
Asraf Uddin Ahmed <asraf@dginnovationlab.com>
2018-09-05 12:48:25 +0600

    JWT token implemented

--- spring-boot-hibernate-auth/src/main/java/com/asraf/config/AuthorizationServerConfig.java
+++ spring-boot-hibernate-auth/src/main/java/com/asraf/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.asraf.config;
 
+import java.util.Arrays;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -15,11 +19,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
+@Order(2)
 @Import(WebSecurityConfig.class)
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
      
@@ -34,10 +43,33 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public TokenStore tokenStore() {
         return new JdbcTokenStore(dataSource);
+        //return new JwtTokenStore(accessTokenConverter());
     }
+    
     @Bean
     public OAuth2AccessDeniedHandler oauthAccessDeniedHandler() {
         return new OAuth2AccessDeniedHandler();
+    }
+    
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
+ 
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+    
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
     }
     
     @Override
@@ -55,7 +87,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-    	endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+    	TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(
+          Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+     
+        endpoints.tokenStore(tokenStore())
+                 .tokenEnhancer(tokenEnhancerChain)
+                 .authenticationManager(authenticationManager);
     }
     
 }
--- spring-boot-hibernate-auth/src/main/java/com/asraf/config/ResourceServerConfiguration.java
+++ spring-boot-hibernate-auth/src/main/java/com/asraf/config/ResourceServerConfiguration.java
@@ -1,11 +1,13 @@
 package com.asraf.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 import com.asraf.constants.ScopeTypes;
 
@@ -13,12 +15,15 @@ import com.asraf.constants.ScopeTypes;
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+	@Autowired
+    private DefaultTokenServices tokenServices;
+    
     private static final String RESOURCE_ID = "resource-server-rest-api";
     private static final String SECURED_PATTERN = "/**";
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(RESOURCE_ID);
+        resources.resourceId(RESOURCE_ID).tokenServices(tokenServices);
     }
 
     @Override
@@ -30,4 +35,5 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                 .antMatchers(HttpMethod.DELETE, SECURED_PATTERN).access(ScopeTypes.DELETE)
                 .anyRequest().access(ScopeTypes.READ);
     }
+       
 }
